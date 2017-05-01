f2ed49df0a35bc803d8795b9528fbeb2c004e4b5
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-05-01 11:20:08 -0400

    added pipeline_test.py

---
+++ tests/pipeline_test.py
@@ -0,0 +1,6 @@
+import clarityviz as cl
+# token = 's275_to_ara3'
+token = 's3617_to_ara3'
+cert_path = 'userToken.pem'
+# cl.analysis.get_registered(token, cert_path)
+cl.analysis.run_pipeline(token, cert_path, 5)
